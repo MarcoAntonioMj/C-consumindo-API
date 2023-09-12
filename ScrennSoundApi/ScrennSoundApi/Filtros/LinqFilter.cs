dfffa284e3e9e0cfd31cb96644c74de7278eb2dc
Marco Antonio <marcotonho38@gmail.com>
2023-09-12 20:24:09 -0300

    FEAT : criando arquivo Json pelo c#

--- ScrennSoundApi/ScrennSoundApi/Filtros/LinqFilter.cs
+++ ScrennSoundApi/ScrennSoundApi/Filtros/LinqFilter.cs
@@ -31,4 +31,30 @@ internal class LinqFilter
             Console.WriteLine($"- {musica.Nome}");
         }
     }
+    public static void FiltrarMusicasPeloAno(List<Musica> musicas, int ano)
+    {
+        var musicasDoAno = musicas.Where(musica => musica.Ano == ano)
+            .OrderBy(musicas => musicas.Nome) // ordena as músicas pelo nome
+    .Select(musicas => musicas.Nome) // seleciona apenas o nome das músicas
+            .Distinct() // remove as duplicidades
+            .ToList(); // converte o resultado em uma lista
+
+        Console.WriteLine($"Músicas de {ano}");
+        foreach (var musica in musicasDoAno)
+        {
+            Console.WriteLine($"- {musica}");
+        }
+    }
+
+    internal static void FltrarMusicasEmCSharp(List<Musica> musicas)
+    {
+        var musicasEmCSharp = musicas.Where(musica => musica.Tonalidade.Equals("C#"))
+        .Select(musica => musica.Nome)
+        .ToList();
+        Console.WriteLine("Musicas em c#");
+        foreach (var musica in musicasEmCSharp)
+        {
+            Console.WriteLine($"- {musica}");
+        }
+    }
 }
--- ScrennSoundApi/ScrennSoundApi/Program.cs
+++ ScrennSoundApi/ScrennSoundApi/Program.cs
@@ -8,10 +8,26 @@ using (HttpClient client = new HttpClient())
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        //LinqFilter.FltrarMusicasEmCSharp(musicas);
+
+        // musicas[0].ExibirDetalhesDaMusica();
         //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas)
         //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
         //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
-        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+        //LinqFilter.FiltrarMusicasPeloAno(musicas, 2012);
+
+        //var musicasPreferidasDoDaniel = new MusicasPreferidas("Daniel");
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[3]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[77]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[6]);
+        //musicasPreferidasDoDaniel.AdicionarMusicasFavoritas(musicas[1467]);
+
+        //musicasPreferidasDoDaniel.ExibirMusicasFavoritas();
+
+        //musicasPreferidasDoDaniel.GerarArquivosJson();
 
     }
     catch (Exception ex)
